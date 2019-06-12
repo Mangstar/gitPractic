24828580ac0f4efdbe873263725369a45ff12ab3
Mike <MishkaMihan@yandex.ru>
2019-06-12 14:30:47 +0300

    Исправлен не работающий основной скрипт сайта (main.js). Добавлено и стилизовано вспомогательное меню в шапке сайта.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function _addListener(target, event, callback){
-	target.addEventLister(event, callback);
+	target.addEventListener(event, callback);
 }
 
 function _removeListener(target, event, callback){
-	target.removeEventLister(event, callback);
+	target.removeEventListener(event, callback);
 }
 
 _addListener(window, 'load', function(){
